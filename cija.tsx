1aec1e7296f66b2342a56e07d31b59c0ab2c80b5
Till Bucker <9974667+llit74@users.noreply.github.com>
2021-04-25 14:50:27 +0200

    Add musik

--- cija.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="10_coffee" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="tiles_png/community/cert/imgs/food_and_drinks/10_coffee.png" width="32" height="32"/>
-</tileset>
